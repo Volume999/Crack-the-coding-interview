c99d740af18b6d12414da7870b8ab6d12cee3b2a
Volume999 <Volume99917@gmail.com>
2024-01-14 14:31:51 +0100

    C2: Loop detection

--- Chapter2/loop_detection.py
+++ Chapter2/loop_detection.py
@@ -4,10 +4,30 @@ class Node:
         self.next = None
 
 def solve(input):
-    pass
+    head = input
+    slow = head 
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    if not fast or not fast.next:
+        return None
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return slow.data
 
 def generate_input():
-    pass
+    head = Node('A')
+    head.next = Node('B')
+    head.next.next = Node('C')
+    head.next.next.next = Node('D')
+    head.next.next.next.next = Node('E')
+    head.next.next.next.next.next = head.next.next
+    return [head]
 
 def main():
     input = generate_input()
